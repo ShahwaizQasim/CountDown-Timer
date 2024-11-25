ed0b29c4dca652a11d6d80a5d5327b6ef8a36b31
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-25 10:33:44 -0800

    user value store

--- src/Components/CountDown/countDown.jsx
+++ src/Components/CountDown/countDown.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button/button'
 
 function CountDown() {
+  const [inputValue, setInputValue] = useState("");
+  console.log("inputValue", inputValue);
+
   return (
     <div className='h-dvh flex justify-center flex-col gap-4 items-center'>
-        <h1 className='text-3xl font-bold'>CountDown Timer</h1>
-        <input placeholder='Enter Times in Minute...' type='number' className='border-2 py-2 mt-5 px-3 w-[300px]' />
-        <h2 className='text-4xl'>00:00</h2>
-        <div className='flex gap-3'>
-        <Button label={"Start"}/>
-        <Button label={"Pause"}/>
-        <Button label={"Reset"}/>
-        </div>
+      <h1 className='text-4xl font-semibold'>CountDown Timer</h1>
+      <input placeholder='Enter Times in Minute...' type='number'
+        value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='border-2 py-2 mt-5 px-3 w-[300px]' />
+      <h2 className='text-4xl'>00:00</h2>
+      <div className='flex gap-3'>
+        <Button label={"Start"} />
+        <Button label={"Pause"} />
+        <Button label={"Reset"} />
+      </div>
     </div>
   )
 }
