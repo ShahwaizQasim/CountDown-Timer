b3e3396dfe71243b6c839de22f00db20a9346abf
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-25 03:32:58 -0800

    countdown timer ready

---
+++ src/Components/Button/button.jsx
@@ -0,0 +1,8 @@
+
+function Button({label}) {
+  return (
+    <button className="py-2 px-8 bg-black text-white rounded-[8px] border-2 text-[16px]">{label}</button>
+  )
+}
+
+export default Button
--- src/Components/CountDown/countDown.jsx
+++ src/Components/CountDown/countDown.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Button from '../Button/button'
 
 function CountDown() {
   return (
-    <div className='h-dvh flex justify-center items-center'>
+    <div className='h-dvh flex justify-center flex-col gap-4 items-center'>
         <h1 className='text-3xl font-bold'>CountDown Timer</h1>
+        <input placeholder='Enter Times in Minute...' type='number' className='border-2 py-2 mt-5 px-3 w-[300px]' />
+        <h2 className='text-4xl'>00:00</h2>
+        <div className='flex gap-3'>
+        <Button label={"Start"}/>
+        <Button label={"Pause"}/>
+        <Button label={"Reset"}/>
+        </div>
     </div>
   )
 }
