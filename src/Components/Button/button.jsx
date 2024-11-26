dcfcdf8af82f0da0f3ec2d403d52660522d44c99
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-11-26 03:41:36 -0800

    functionality done

--- src/Components/Button/button.jsx
+++ src/Components/Button/button.jsx
@@ -1,7 +1,7 @@
 
-function Button({label}) {
+function Button({label,onClick}) {
   return (
-    <button className="py-2 px-8 bg-black text-white rounded-[8px] border-2 text-[16px]">{label}</button>
+    <button className="py-2 px-8 bg-black text-white rounded-[8px] border-2 text-[16px]" onClick={onClick}>{label}</button>
   )
 }
 
--- src/Components/CountDown/countDown.jsx
+++ src/Components/CountDown/countDown.jsx
@@ -1,20 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Button from '../Button/button'
 
 function CountDown() {
-  const [inputValue, setInputValue] = useState("");
-  console.log("inputValue", inputValue);
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPause, setIsPause] = useState(false);
+  let intervalRef = useRef(null);
+
+  const handleInput = (e) => {
+        setTime(parseInt(e.target.value * 60)) // yahan user se input number lekr minute ko sec me convert kiya hai
+  }
+
+  const formatTime = () => {
+    const min = String(Math.floor(time/60)).padStart(2,'0'); // yahan sec ko minute me convert kiya hai
+    const sec = String(time%60).padStart(2,'0'); // min ko phr sec me convert krdia
+    return `${min} : ${sec}`;
+  }
+
+  const handleStart = () => {
+     setIsActive(true);
+     setIsPause(false);
+    }
+
+  useEffect(()=>{
+    if (isActive && !isPause && time > 0) {
+       intervalRef.current = setInterval(()=>{
+          setTime((prev)=> prev -1)
+       },1000)
+    }else if( time == 0){
+      clearInterval(intervalRef.current);
+      setIsActive(false)
+      alert('Time is up')
+    }
+    return () => clearInterval(intervalRef.current);
+   }, [isActive, isPause, time])
+
+   const handlePause = () => {
+      setIsPause(!isPause)
+   }
+
+   const handleReset = () => {
+    clearInterval(intervalRef.current)
+    setIsActive(false)
+    setIsPause(false)
+    setTime(0)
+ }
 
   return (
     <div className='h-dvh flex justify-center flex-col gap-4 items-center'>
       <h1 className='text-4xl font-semibold'>CountDown Timer</h1>
       <input placeholder='Enter Times in Minute...' type='number'
-        value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='border-2 py-2 mt-5 px-3 w-[300px]' />
-      <h2 className='text-4xl'>00:00</h2>
+        onChange={handleInput} className='border-2 py-2 mt-5 px-3 w-[300px]' />
+      <h2 className='text-4xl'>{formatTime()}</h2>
       <div className='flex gap-3'>
-        <Button label={"Start"} />
-        <Button label={"Pause"} />
-        <Button label={"Reset"} />
+        <Button label={"Start"} onClick={handleStart} />
+        <Button label={"Pause"} onClick={handlePause} />
+        <Button label={"Reset"} onClick={handleReset} />
       </div>
     </div>
   )
